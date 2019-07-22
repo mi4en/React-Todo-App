83d32b0618b215ff4d023ec938964d1add9c24ae
mi4en <marionikolov87@gmail.com>
2019-07-22 20:47:07 +0300

    fix checkboxes state;

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,18 +7,24 @@ export class TodoItem extends Component {
 			background: '#f4f4f4',
 			padding: '10px',
 			borderBottom: '1px #ccc dotted',
-			textDecoration: this.props.todo.completed ? 'line-through' : 'none'
-		}
-	}
+			textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+		};
+	};
 
 	render() {
-		const { id, title } = this.props.todo
+		const { id, title, completed } = this.props.todo;
 		return (
 			<div style={this.getStyle()}>
 				<p>
-					<input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
+					<input
+						type="checkbox"
+						checked={completed}
+						onChange={this.props.markComplete.bind(this, id)}
+					/>
 					{title}
-					<button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>x</button>
+					<button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>
+						x
+					</button>
 				</p>
 			</div>
 		);
@@ -37,14 +43,14 @@ const btnStyle = {
 	padding: '5px 9px',
 	borderRadius: '50%',
 	cursor: 'pointer',
-	float: 'right'
-}
+	float: 'right',
+};
 
 // PropTypes
 TodoItem.propTypes = {
 	todo: PropTypes.object.isRequired,
 	markComplete: PropTypes.func.isRequired,
-	delTodo: PropTypes.func.isRequired
-}
+	delTodo: PropTypes.func.isRequired,
+};
 
 export default TodoItem;
